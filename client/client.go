db0b253fc8efb7ed4b45e3e20ca11f2915b31f95
Alexey Tsvetkov <type.inference@gmail.com>
2014-12-26 10:21:36 +0300

    Simple message passing impementation

---
+++ client/client.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+	"io"
+)
+
+type Client struct {
+	id     int
+	reader *bufio.Reader
+	writer *bufio.Writer
+}
+
+func (client *Client) Run() {
+	msg := fmt.Sprintf("Hi! My name is %d\n", client.id)
+	_, err := client.writer.WriteString(msg)
+	client.writer.Flush()
+
+	if err != nil {
+		log.Printf("Client %d write error: %s", client.id, err)
+		return
+	}
+
+	response := ""
+
+	for {
+		line, err := client.reader.ReadString('\n')
+
+		if err == nil {
+			response += line
+		} else if err == io.EOF {
+			break
+		} else {
+			log.Printf("Client %d got error in response: %s", client.id, err)
+			return
+		}
+	}
+
+	log.Printf("Client %d got: %s", client.id, response)
+	time.Sleep(5 * time.Second)
+}
+
+func RunClient(id int, out chan<- int) {
+	conn, err := net.Dial("tcp", "localhost:5432")
+
+	if err != nil {
+		log.Printf("Could not create client %d: %s", id, err)
+		return
+	}
+
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
+
+	client := &Client{
+		id:     id,
+		reader: reader,
+		writer: writer,
+	}
+
+	client.Run()
+	out <- id
+}
+
+const maxClients int = 10
+
+func main() {
+	id := 0
+	clients := 0
+	out := make(chan int, maxClients)
+	
+	for {
+		for clients < maxClients {
+			id += 1
+			clients += 1
+			go RunClient(id, out)
+		}
+
+		select {
+		case <-out:
+			clients -= 1
+		}
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+const maxJobsBuffer int = 50
+const maxConnsBuffer int = 50
+
+type Job struct {
+	msg string
+}
+
+type Result struct {
+	msg string
+	job *Job
+}
+
+type Worker struct {
+	id      int
+	jobs    <-chan *Job
+	results chan<- *Result
+}
+
+func (worker *Worker) Run() {
+	for {
+		select {
+		case job := <-worker.jobs:
+			result := &Result{
+				msg: fmt.Sprintf("Worker %d: %s\n", worker.id, job.msg),
+				job: job,
+			}
+			worker.results <- result
+		}
+	}
+}
+
+type Server struct {
+	jobConn map[*Job]net.Conn
+	jobs      chan<- *Job
+	results   <-chan *Result
+	conns     <-chan net.Conn
+	workers   []*Worker
+}
+
+func (serv *Server) Listen() {
+	for _, worker := range serv.workers {
+		go worker.Run()
+	}
+
+	for {
+		select {
+		case conn := <-serv.conns:
+			reader := bufio.NewReader(conn)
+			line, _ := reader.ReadString('\n')
+			job := &Job{
+				msg: line,
+			}
+			serv.jobConn[job] = conn
+			serv.jobs <- job
+		case result := <-serv.results:
+			if conn, ok := serv.jobConn[result.job]; ok {
+				writer := bufio.NewWriter(conn)
+				log.Print(result.msg)
+				writer.WriteString(result.msg)
+				conn.Close()
+			} else {
+				log.Fatal("Could not find connection for job")
+			}
+		}
+	}
+}
+
+func NewServer(conns chan net.Conn, workersCount int) *Server {
+	jobs := make(chan *Job, maxJobsBuffer)
+	results := make(chan *Result, maxJobsBuffer)
+	var workers []*Worker
+
+	for i := 0; i < workersCount; i++ {
+		worker := &Worker{
+			id:      i,
+			jobs:    jobs,
+			results: results,
+		}
+
+		workers = append(workers, worker)
+	}
+
+	serv := &Server{
+		jobConn: make(map[*Job]net.Conn),
+		jobs:      jobs,
+		results:   results,
+		conns:     conns,
+		workers:   workers,
+	}
+
+	return serv
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:5432")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
+	conns := make(chan net.Conn, maxConnsBuffer)
+	serv := NewServer(conns, 5)
+	go serv.Listen()
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		conns <- conn
+	}
+}
